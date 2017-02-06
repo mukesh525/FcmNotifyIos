f0af8ceaf8854a723c901b007c14502975fadefc
Mukesh <mukeshjha2007@gmail.com>
2017-02-06 10:58:53 +0530

    payload fix

--- FCMNotification/ViewController.swift
+++ FCMNotification/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let token = FIRInstanceID.instanceID().token() {
+           print("Token \(token)")
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    @IBAction func handleLogTokenTouch(_ sender: UIButton) {
+//        // [START get_iid_token]
+//        let token = FIRInstanceID.instanceID().token()
+//        print("InstanceID token: \(token!)")
+//        // [END get_iid_token]
+//    }
+    
+    @IBAction func handleSubscribeTouch(_ sender: UIButton) {
+        // [START subscribe_topic]
+        FIRMessaging.messaging().subscribe(toTopic: "/topics/news")
+        print("Subscribed to news topic")
+        // [END subscribe_topic]
     }
-
 
 }
 
